84bddc9c06a7806af7ebd9c53729ec85e9c20b7d
sougeo73 <76899495+sougeo73@users.noreply.github.com>
2021-01-16 16:21:40 +0200

    Add files via upload

---
+++ src/main/java/com/example/android/miwok/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.android.miwok;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Set the content of the activity to use the activity_main.xml layout file
+        setContentView(R.layout.activity_main);
+
+        public void openNumbersList (View view) {
+            Intent i = new Intent(this, NumbersActivity.class);
+            startActivity(i);
+        }
+
+
+    }
+}
